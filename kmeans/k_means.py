be47667ca163ef963d9193975a5d303c0310cde7
kai <sucre1136@gmail.com>
2024-02-07 08:20:43 +0000

    kmeans start

--- kmeans/k_means.py
+++ kmeans/k_means.py
@@ -7,29 +7,19 @@ class KMeans:
         self.num_clusters = num_clusters
 
     def train(self, max_iterations):
+        # 1 get random centroids
         centroids = KMeans.centroids_init(self.data, self.num_clusters)
-
+        # start to train data
         num_examples = self.data.shape[0]
         closest_centroids_ids = np.empty((num_examples, 1))
         for _ in range(max_iterations):
+            # get the distance form centroids to other sample points
             closest_centroids_ids = KMeans.centroids_find_closest(self.data, centroids)
 
+
     @staticmethod
-    def centroids_init(self, data, num_clusters):
+    def centroids_init(data, num_clusters):
         num_examples = data.shape[0]
         random_ids = np.random.permutation(num_examples)
-        centroids = data[random_ids[:num_examples], :]
+        centroids = data[random_ids[:num_clusters], :]
         return centroids
-
-    def centroids_find_closest(self, data, centroids):
-        num_examples = self.data.shape[0]
-        num_centroids = centroids.shape[0]
-        closest_centroids_ids = np.zeros((num_examples, 1))
-        for example_index in range(num_examples):
-            distance = np.zeros(num_centroids, 1)
-            for centroid_index in range(num_centroids):
-                distance_diff = data[example_index, :] - centroids[centroid_index]
-                distance[centroid_index] = np.sum((distance_diff ** 2))
-            closest_centroids_ids[example_index] = np.argmin(distance)
-
-        return  closest_centroids_ids
